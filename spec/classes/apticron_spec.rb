9d83e45656c1405cdd07f3fcee4063f801735780
Stephen Hoekstra <stephenhoekstra@gmail.com>
2014-11-02 23:39:28 +0000

    added :metadata rake task and improved rspec tests

--- spec/classes/apticron_spec.rb
+++ spec/classes/apticron_spec.rb
@@ -1,103 +1,92 @@
 require 'spec_helper'
 
 describe 'apticron', :type => :class do
-  context "osfamily => 'Debian'" do
-    let(:facts) {{ :osfamily => 'Debian' }}
+  let :default_params do
+    { :mail_to => 'apticron@example.com' }
+  end
 
-    describe "with mail_to => 'apticron@example.com'" do
-      let(:params) {{ :mail_to => 'apticron@example.com' }}
+  context "supported OS families" do
+    describe "Debian" do
+      let(:facts) {{ :osfamily => 'Debian' }}
 
-      it { should compile.with_all_deps }
+      describe "when $mail_to is 'apticron@example.com'" do
+        let :params do default_params.merge({}) end
 
-      it { should contain_class('apticron::params') }
-      it { should contain_class('apticron::install').that_comes_before('apticron::config') }
-      it { should contain_class('apticron::config').that_notifies('apticron::cron') }
-      it { should contain_class('apticron::cron').that_notifies('apticron') }
-      it { should contain_class('apticron') }
-      it { should contain_file('/etc/apticron/apticron.conf').with(
-        'ensure' => 'present',
-        'owner'  => 'root',
-        'group'  => 'root',
-        'mode'   => '0644'
-      )}
-      it { should contain_file("/etc/apticron/apticron.conf").with_content %r{^EMAIL="apticron@example.com"\n} }
-      it { should contain_package('apticron').with_ensure('present') }
+        it { should compile.with_all_deps }
 
-      describe "custom_subject => 'apticron custom subject'" do
-        let(:params) {{ :mail_to => 'apticron@example.com', :custom_subject => "apticron custom subject" }}
-
-        it do
-          should contain_file('/etc/apticron/apticron.conf').with_content(/^CUSTOM_SUBJECT="apticron custom subject"$/)
-        end
+        it { should contain_class('apticron::params') }
+        it { should contain_class('apticron::install').that_comes_before('apticron::config') }
+        it { should contain_class('apticron::config').that_notifies('apticron::cron') }
+        it { should contain_class('apticron::cron').that_notifies('apticron') }
+        it { should contain_class('apticron') }
+        it { should contain_file('/etc/apticron/apticron.conf').with(
+          'ensure' => 'present',
+          'owner'  => 'root',
+          'group'  => 'root',
+          'mode'   => '0644'
+        )}
+        it { should contain_file("/etc/apticron/apticron.conf").with_content %r{^EMAIL="apticron@example.com"\n} }
+        it { should contain_package('apticron').with_ensure('present') }
       end
 
-      describe "mail_from => 'root@server.example.com'" do
-        let(:params) {{ :mail_to => 'apticron@example.com', :mail_from => "root@server.example.com" }}
+      [
+        {
+          :attr  => 'mail_from',
+          :value => 'root@server.example.com',
+          :match => [/^CUSTOM_FROM="root@server.example.com"$/]
+        },
+        {
+          :attr  => 'custom_subject',
+          :value => 'apticron custom subject',
+          :match => [/^CUSTOM_SUBJECT="apticron custom subject"$/]
+        },
+        {
+          :attr  => 'diff_only',
+          :value => true,
+          :match => [/^DIFF_ONLY="1"$/]
+        },
+        {
+          :attr  => 'diff_only',
+          :value => false,
+          :match => [/^DIFF_ONLY="0"$/]
+        },
+        {
+          :attr  => 'notify_holds',
+          :value => true,
+          :match => [/^NOTIFY_HOLDS="1"$/]
+        },
+        {
+          :attr  => 'notify_holds',
+          :value => false,
+          :match => [/^NOTIFY_HOLDS="0"$/]
+        },
+        {
+          :attr  => 'notify_new',
+          :value => true,
+          :match => [/^NOTIFY_NEW="1"$/]
+        },
+        {
+          :attr  => 'notify_new',
+          :value => false,
+          :match => [/^NOTIFY_NEW="0"$/]
+        }
+      ].each do |param|
+        describe "when \$#{param[:attr]} is #{param[:value]}" do
+          let :params do default_params.merge({ param[:attr].to_sym => param[:value] }) end
 
-        it do
-          should contain_file('/etc/apticron/apticron.conf').with_content(/^CUSTOM_FROM="root@server.example.com"$/)
-        end
-      end
-
-      describe "diff_only" do
-        context "=> true" do
-          let(:params) {{ :mail_to => 'apticron@example.com', :diff_only => true }}
-
-          it do
-            should contain_file('/etc/apticron/apticron.conf').with_content(/^DIFF_ONLY="1"$/)
-          end
-        end
-
-        context "=> false" do
-          let(:params) {{ :mail_to => 'apticron@example.com', :diff_only => false }}
-
-          it do
-            should contain_file('/etc/apticron/apticron.conf').with_content(/^DIFF_ONLY="0"$/)
-          end
-        end
-      end
-
-      describe "notify_holds" do
-        context "=> true" do
-          let(:params) {{ :mail_to => 'apticron@example.com', :notify_holds => true }}
-
-          it do
-            should contain_file('/etc/apticron/apticron.conf').with_content(/^NOTIFY_HOLDS="1"$/)
-          end
-        end
-
-        context "=> false" do
-          let(:params) {{ :mail_to => 'apticron@example.com', :notify_holds => false }}
-
-          it do
-            should contain_file('/etc/apticron/apticron.conf').with_content(/^NOTIFY_HOLDS="0"$/)
-          end
-        end
-      end
-
-      describe "notify_new" do
-        context "=> true" do
-          let(:params) {{ :mail_to => 'apticron@example.com', :notify_new => true }}
-
-          it do
-            should contain_file('/etc/apticron/apticron.conf').with_content(/^NOTIFY_NEW="1"$/)
-          end
-        end
-
-        context "=> false" do
-          let(:params) {{ :mail_to => 'apticron@example.com', :notify_new => false }}
-
-          it do
-            should contain_file('/etc/apticron/apticron.conf').with_content(/^NOTIFY_NEW="0"$/)
+          it "should set #{param[:attr]} to \'#{param[:value]}\'" do
+            should contain_file('/etc/apticron/apticron.conf').with_content(param[:match])
           end
         end
       end
     end
   end
 
-  context "osfamily => 'Solaris' (unsupported operating system)" do
-    let(:facts) {{ :osfamily => 'Solaris' }}
+  context "unsupported OS family" do
+    describe "Solaris" do
+      let(:facts) {{ :osfamily => 'Solaris' }}
 
-    it { expect { should contain_package('apticron') }.to raise_error(Puppet::Error, /apticron module not supported on Solaris/) }
+      it { expect { should contain_package('apticron') }.to raise_error(Puppet::Error, /apticron module not supported on Solaris/) }
+    end
   end
 end
